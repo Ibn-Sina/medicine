acb0dbfe05108de7ee2e17ee1e3c12224d77bc3c
IshaqAlathamneh <ishaqalathamneh@gmail.com>
2021-02-28 10:43:49 +0200

    main page

---
+++ js/app.js
@@ -0,0 +1,19 @@
+window.onscroll = function() {myFunction()};
+function myFunction(){
+// let headEl= document.getElementById("header");
+// headEl.style.backgroundColor="#f6f5f5";
+if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+    // document.getElementById("header").style.fontSize = "15px";
+    document.getElementById("header").style.backgroundColor = "#393e46";
+    document.getElementById("header").style.transitionDuration = "300ms";
+    // document.getElementById("aOne").style.color = "#98ded9";
+    // document.getElementById("aTwo").style.color = "#98ded9";
+    // document.getElementById("aThree").style.color = "#98ded9";
+    // document.getElementById("aFour").style.color = "#98ded9";
+  } else {
+    // document.getElementById("header").style.fontSize = "20px";
+    document.getElementById("header").style.backgroundColor = "";
+    document.getElementById("header").style.transitionDuration = "300ms";
+    document.getElementById("aOne").style.color = "white";
+  }
+}
