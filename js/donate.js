d035c3e77ed53595eb28a9d55b212ce2ba92cee3
IshaqAlathamneh <ishaqalathamneh@gmail.com>
2021-03-01 20:16:02 +0200

    form edit

--- js/donate.js
+++ js/donate.js
@@ -95,3 +95,71 @@ let donateD = prompt("to donate by money enter 1  or 2 for medicine");
 getData();
 // runder();
 
+
+let myForm = document.getElementById('myForm')
+let donarName ,medicineName , checkBox , imgSource , amount = 0;
+let newArr = [];
+// let medicineNames = [];
+// let checkBoxs = [];
+// let imgSources = [];
+// let amounts = [];
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+    let submitBtn = document.getElementById('submit')
+    
+    myForm.addEventListener('submit',function newLocal(event){
+        event.preventDefault();
+        console.log(event);
+        donarName = event.target.userName.value;
+        medicineName = event.target.medicineName.value;
+        checkBox = event.target.hideName.checked;
+        imgSource = event.target.source.value;
+        amount = parseInt(event.target.amount.value);
+        new NewDon (donarName, medicineName, checkBox, imgSource, amount)
+        let newData = JSON.stringify(newArr);
+        localStorage.setItem('new donation',newData)
+        swal("Thank You!", "Maybe a human will survive cause of your donation", 'success');
+        newRender()
+        myForm.removeEventListener('submit',newLocal)
+        closeForm()
+    })
+    
+  }
+  function setLocalFun (){
+
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
+// myForm.addEventListener('pointerleave', closeForm)
+function newRender() {
+   let cont1 = document.getElementById('donarDi');
+   let gd = localStorage.getItem('new donation')
+   let list2 = JSON.parse(gd);
+   for(let i = 0; i < list2.length ; i++){
+   let cont3 = document.createElement('div')
+   cont1.appendChild(cont3)
+   cont3.id = 'donarDiv'
+   let myTitle = document.createElement('h3')
+   cont3.appendChild(myTitle);
+   if (list2[i].checkBox){
+       list2[i].name = 'A person';
+   }
+   myTitle.textContent = `${list2[i].name} donated ${list2[i].amount} piece(s) of (${list2[i].medicine})`
+   let imgNew = document.createElement('img')
+   cont3.appendChild(imgNew)
+   imgNew.id = 'donarImg'
+   imgNew.src = list2[i].imgSource;
+}
+
+}
+function NewDon (donarName, medicineName, checkBox, imgSource, amount){
+    this.name = donarName;
+    this.medicine = medicineName;
+    this.checkBox = checkBox;
+    this.imgSource = imgSource;
+    this.amount = amount;
+    newArr.push(this)
+   }
+   newRender()
