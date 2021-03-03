563bb8673a8554bd69da91d3aca5f00f933a4a83
IshaqAlathamneh <ishaqalathamneh@gmail.com>
2021-03-03 20:20:00 +0200

    Final commit in this project

--- js/donate.js
+++ js/donate.js
@@ -1,13 +1,118 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
 let medicineInNeed2=[];
 let container = document.getElementById('container');
 let container2 = document.getElementById('container2');
+let myForm = document.getElementById('myForm');
+let medicineName , imgSource , amount = 0;
 let i=0;
-//structure Div :-
-//card
-//imgBox {img }
-//content{H2 , Button }
-
+const donetorform=document.getElementById('Dinfo');
+donetorform.addEventListener('submit',addToForm2);
+let arrayForm2 = [];
 function MedicineInNeed(name , cost , source ,info){
+  this.name = name;
+  this.cost = cost;
+  this.source = source;
+  this.info=info;
+  runder();
+}
+function runder(){
+  // create div card
+  let divCard=document.createElement('div');
+  divCard.setAttribute('class', 'card');
+  // create div ImgBox
+  let divImgBox=document.createElement('div');
+  divImgBox.setAttribute('class', 'imgBx');
+  //creat img Element  & give it src
+  let imgEl=document.createElement('img');
+  imgEl.setAttribute('src' , medicineInNeed2[i].source);
+  // create div Contant
+  let divContant =document.createElement('div');
+  divContant.setAttribute('class','content');
+  //create h2 Element & fell it with Name
+  let h2Content = document.createElement('h2');
+  h2Content.textContent = medicineInNeed2[i].name;
+  let pContent = document.createElement('p');
+  pContent.textContent = medicineInNeed2[i].info;
+  let divbox =document.createElement('div');
+  divbox.setAttribute('class','box');
+  // let aContent = document.createElement('a');
+  // aContent.setAttribute('href','#popup1');
+  // aContent.textContent='Donate Now';
+  // <div class="box">
+  // <a class="button" href="#popup1">Let me Pop up</a>
+  // </div>
+  //creat Button Element
+  let buttonEl=document.createElement('button');
+  buttonEl.setAttribute('type','button');
+  buttonEl.setAttribute('id', `${i}`);
+  buttonEl.textContent='donate for it ';
+  buttonEl.addEventListener('click',addToDonate);
+  //fell imgBox Div with imgEl
+  divImgBox.appendChild(imgEl);
+  //fell content
+  divContant.appendChild(h2Content);
+  divContant.appendChild(pContent);
+  //push div ImgBox & div Contant inside divCard
+  divCard.appendChild(divImgBox);
+  divCard.appendChild(divContant);
+  divCard.appendChild(divbox);
+  divCard.appendChild(buttonEl);
+  if(i%2===0){
+    container.appendChild(divCard);
+    if(i>=2){
+      divCard.setAttribute('data-aos','fade-right');
+      divCard.style.transition= '0.3s ease-in-out';
+    }
+  }
+  else{
+    container2.appendChild(divCard);
+    if(i>=2){
+      divCard.setAttribute('data-aos','fade-left');
+      divCard.style.transition= '0.3s ease-in-out';
+    }
+  }
+  i++;
+}
+function getData(){
+  let gettingData = localStorage.getItem('Medicine');
+  let list1 = JSON.parse(gettingData);
+  if(list1){
+    medicineInNeed2 = list1;
+    runder();
+  }
+}
+function addToDonate(){
+  document.location.href='#popup1';
+}
+function openForm() {
+  document.getElementById('myForm').style.display = 'block';
+  myForm.addEventListener('submit',function newLocal(event){
+    event.preventDefault();
+    console.log(event);
+    medicineName = event.target.medicineName.value;
+    imgSource = event.target.source.value;
+    amount = parseInt(event.target.amount.value);
+    new MedicineToDonate ( medicineName,amount, imgSource, 'info');
+    swal('Thank You!', 'Maybe a human will survive cause of your donation', 'success');
+    myForm.removeEventListener('submit',newLocal);
+    closeForm();
+  });
+}
+function closeForm() {
+  document.getElementById('myForm').style.display = 'none';
+}
+function gitdonation(){
+  for (let y=0; y<medicineInNeed2.length;y++){
+    console.log(medicineInNeed2[y]);
+    let n =medicineInNeed2[y].name;
+    let c =medicineInNeed2[y].cost;
+    let s =medicineInNeed2[y].source;
+    let i ='Info';
+    new MedicineInNeed(n, c, s, i);
+  }
+}
+function MedicineToDonate(name , cost , source ,info){
   this.name = name;
   this.cost = cost;
   this.source = source;
@@ -15,185 +120,28 @@ function MedicineInNeed(name , cost , source ,info){
   medicineInNeed2.push(this);
   runder();
 }
-
-
-
-
-function runder(){
-
-  // create div card
-  let divCard=document.createElement('div');
-  divCard.setAttribute('class', 'card');
-
-  // create div ImgBox
-  let divImgBox=document.createElement('div');
-  divImgBox.setAttribute('class', 'imgBx');
-
-  //creat img Element  & give it src
-  let imgEl=document.createElement('img');
-  imgEl.setAttribute('src' , medicineInNeed2[i].source);
-
-  // create div Contant
-  let divContant =document.createElement('div');
-  divContant.setAttribute('class','content');
-
-  //create h2 Element & fell it with Name
-  let h2Content = document.createElement('h2');
-  h2Content.textContent = medicineInNeed2[i].name;
-
-
-  let pContent = document.createElement('p');
-  pContent.textContent = medicineInNeed2[i].info;
-
-  let divbox =document.createElement('div');
-  divbox.setAttribute('class','box');
-
-  let aContent = document.createElement('a');
-  aContent.setAttribute('href','#popup1');
-  aContent.textContent='Donate Now';
-
-  // <div class="box">
-  // <a class="button" href="#popup1">Let me Pop up</a>
-  // </div>
-  divbox.appendChild(aContent);
-
-  //creat Button Element
-  let buttonEl=document.createElement('button');
-  buttonEl.setAttribute('type','button');
-
-  buttonEl.setAttribute('id', `${i}`);
-  buttonEl.textContent='donate for it ';
-  buttonEl.addEventListener('click',addToDonate);
-
-  //fell imgBox Div with imgEl
-  divImgBox.appendChild(imgEl);
-
-  //fell content
-  divContant.appendChild(h2Content);
-  divContant.appendChild(pContent);
-
-
-  //push div ImgBox & div Contant inside divCard
-  divCard.appendChild(divImgBox);
-  divCard.appendChild(divContant);
-  divCard.appendChild(divbox);
-
-  if(i%2===0){
-    container.appendChild(divCard);
-
-  }
-  else{
-    container2.appendChild(divCard);
-
-  }
-  i++;
+function Form2(donaterNam,idphone, yourLocation,paymentMethod){
+  this.donaterNam = donaterNam;
+  this.location = yourLocation;
+  this.phoneD=idphone;
+  this.paymentMethod=paymentMethod;
+  arrayForm2.push(this);
+  console.log(this);
 }
-
-function getData(){
-
-  let gettingData = localStorage.getItem('Medicine');
-  let list1 = JSON.parse(gettingData);
-
-
-
-  if(list1){
-    medicineInNeed2 = list1;
-
-  }
-
+function addToForm2(event){
+  event.preventDefault();
+  console.log('1');
+  console.log(event);
+  let idName = event.target.nameDoneter.value;
+  let idLocation = event.target.dloc.value;
+  let idphone = event.target.donaterPhone.value;
+  new Form2 (idName , idphone, idLocation ,'cash');
+  document.location.href='#';
+  savedForm();
 }
-function addToDonate(event){
-  let donateD = prompt('to donate by money enter 1  or 2 for medicine');
-  donateD=parseInt(donateD);
-  if (donateD ===1){
-    let aumont =prompt('enter the amount of money');
-    alert('thank you for your donate');
-  }
-  else if (donateD ===2 ) {
-    let medicineND =prompt('enter the name of medicine ');
-    alert('thank you for your donate');
-  }else {
-    alert('enter 1 or 2');
-  }
-  console.log('donated');
+function savedForm(){
+  let data3 = JSON.stringify(arrayForm2);
+  localStorage.setItem('DonaterInformation', data3);
 }
-
 getData();
-for (let l=0; l<medicineInNeed2.length;l++){
-
-  let n =medicineInNeed2[l].name;
-  let c =medicineInNeed2[l].cost;
-  let s =medicineInNeed2[l].source;
-  let i =medicineInNeed2[l].info;
-  MedicineInNeed(n, c, s, i);
-
-}
-
-
-let myForm = document.getElementById('myForm');
-let medicineName  , imgSource , amount = 0;
-// let medicineNames = [];
-// let checkBoxs = [];
-// let imgSources = [];
-// let amounts = [];
-
-
-
-function openForm() {
-  document.getElementById('myForm').style.display = 'block';
-  let submitBtn = document.getElementById('submit');
-
-  myForm.addEventListener('submit',function newLocal(event){
-    event.preventDefault();
-    console.log(event);
-    medicineName = event.target.medicineName.value;
-    imgSource = event.target.source.value;
-    amount = parseInt(event.target.amount.value);
-
-
-    new MedicineInNeed ( medicineName,amount, imgSource, 'info');
-    // let newData = JSON.stringify(newArr);
-    // localStorage.setItem('new donation',newData);
-    swal('Thank You!', 'Maybe a human will survive cause of your donation', 'success');
-
-    myForm.removeEventListener('submit',newLocal);
-    closeForm();
-  });
-
-}
-
-function closeForm() {
-  document.getElementById('myForm').style.display = 'none';
-}
-
-
-// function newRender() {
-//   let cont1 = document.getElementById('donarDi');
-//   let gd = localStorage.getItem('new donation');
-//   let list2 = JSON.parse(gd);
-//   for(let i = 0; i < list2.length ; i++){
-//     let cont3 = document.createElement('div');
-//     cont1.appendChild(cont3);
-//     cont3.id = 'donarDiv';
-//     let myTitle = document.createElement('h3');
-//     cont3.appendChild(myTitle);
-//     if (list2[i].checkBox){
-//       list2[i].name = 'A person';
-//     }
-//     myTitle.textContent = `${list2[i].name} donated ${list2[i].amount} piece(s) of (${list2[i].medicine})`;
-//     let imgNew = document.createElement('img');
-//     cont3.appendChild(imgNew);
-//     imgNew.id = 'donarImg';
-//     imgNew.src = list2[i].imgSource;
-//   }
-
-
-// function NewDon (donarName, medicineName, checkBox, imgSource, amount){
-//   this.name = donarName;
-//   this.medicine = medicineName;
-//   this.checkBox = checkBox;
-//   this.imgSource = imgSource;
-//   this.amount = amount;
-//   newArr.push(this);
-// }
-// newRender();
+gitdonation();
--- js/medicine.js
+++ js/medicine.js
@@ -6,13 +6,12 @@ let i = 0;
 let container = document.getElementById('container');
 let container2 = document.getElementById('container2');
 let btn2=document.getElementById('mainBtn');
+let myForm = document.getElementById('myForm');
+let medicineName , imgSource='http://www.imgworlds.com/wp-content/uploads/2015/12/18-CONTACTUS-HEADER.jpg' ,price=0 , info=' ';
+let arrayForm = [];
 btn2.addEventListener('click',openForm);
 const needform=document.getElementById('neededform');
-
 needform.addEventListener('submit',addToForm);
-
-
-
 function Medicine(name , cost , source ,info){
   this.name = name;
   this.cost = cost;
@@ -20,84 +19,111 @@ function Medicine(name , cost , source ,info){
   this.info=info;
   medicine.push(this);
   runder();
-  savedData();
-
 }
-
-
-
-//structure Div :-
-//card
-//imgBox {img }
-//content{H2 , Button }
 function runder(){
-
   // create div card
   let divCard=document.createElement('div');
   divCard.setAttribute('class', 'card');
-
   // create div ImgBox
   let divImgBox=document.createElement('div');
   divImgBox.setAttribute('class', 'imgBx');
-
   //creat img Element  & give it src
   let imgEl=document.createElement('img');
   console.log(medicine[0]);
   imgEl.setAttribute('src' , medicine[i].source);
-
   // create div Contant
   let divContant =document.createElement('div');
   divContant.setAttribute('class','content');
-
   //create h2 Element & fell it with Name
   let h2Content = document.createElement('h2');
   h2Content.textContent = medicine[i].name;
-
   let pContent = document.createElement('p');
   pContent.textContent = medicine[i].info;
-
-
   //create Button Element
-  ////////////////////////////////////////////////////////////////////////////////////////////////////////
   let buttonEl=document.createElement('button');
   buttonEl.setAttribute('type','button');
-
   buttonEl.setAttribute('id', `${i}`);
   buttonEl.textContent=`i need it ${medicine[i].cost}`;
   // console.log(buttonEl.id);
   buttonEl.addEventListener('click',addToNeed);
-  //////////////////////////////////////////////////////////////////////////////////////////////////////
-
   btnarr.push(`${i}`);
-
   // console.log(btnarr);
   //fell imgBox Div with imgEl
   divImgBox.appendChild(imgEl);
-
   //fell content
   divContant.appendChild(h2Content);
   divContant.appendChild(pContent);
-
   //push div ImgBox & div Contant inside divCard
   divCard.appendChild(divImgBox);
   divCard.appendChild(divContant);
   divCard.appendChild(buttonEl);
   if(i%2===0){
     container.appendChild(divCard);
+    if(i>=2){
+      divCard.setAttribute('data-aos','fade-right');
+      divCard.style.transition= '0.3s ease-in-out';
+    }
   }
   else{
     container2.appendChild(divCard);
-
+    if(i>=2){
+      divCard.setAttribute('data-aos','fade-left');
+      divCard.style.transition= '0.3s ease-in-out';
+    }
   }
-
   i++;
 }
-
 function savedData(){
-
   let data = JSON.stringify(medicineInNeed);
   localStorage.setItem('Medicine', data);
-
+}
+function addToNeed(event){
+  document.location.href='#popup1';
+  event.preventDefault();
+  let a = event.target.id;
+  medicineInNeed.push(medicine[a]);
+  savedForm();
+  savedData();
+}
+function Form(patientNam,idphone, yourLocation){
+  this.patientNam = patientNam;
+  this.location = yourLocation;
+  this.phonepat=idphone;
+  arrayForm.push(this);
+  console.log(this);
+}
+function addToForm(event){
+  event.preventDefault();
+  console.log('1');
+  console.log(event);
+  let idName = event.target.namepatient.value;
+  let idLocation = event.target.ploc.value;
+  let idphone = event.target.patientPhone.value;
+  new Form (idName , idphone, idLocation );
+  document.location.href='#';
+  savedForm();
+}
+function savedForm(){
+  let data2 = JSON.stringify(arrayForm);
+  localStorage.setItem('forminformation', data2);
+}
+function closeForm() {
+  document.getElementById('myForm').style.display = 'none';
+}
+function openForm() {
+  let elem = document.getElementById('myForm');
+  elem.style.display = 'block';
+  myForm.addEventListener('submit',function newLocal(event){
+    event.preventDefault();
+    console.log(event);
+    medicineName = event.target.medicineName.value;
+    imgSource = event.target.source.value;
+    price = parseInt(event.target.price.value);
+    info =event.target.info.value;
+    new Medicine(medicineName , price , imgSource , info);
+    myForm.removeEventListener('submit',newLocal);
+    closeForm();
+  });
 }
 new Medicine('prexal 10' , '36.41 jd' ,'images/153122281_779031189366261_1816601248261593695_n.jpg' , ' The scientific name : Olanzapine used in the treatment of:- Schizophrenia. - Bipolar disorder. - Treatment of mixed mania or acute manic states associated with bipolar or schizophrenia. - Treatment of intractable cases of depression. - Treating depression associated with bipolar disease');
 new Medicine('Aripal 15','77.63 jd','images/154165555_338485370823778_1229647415121863017_n.jpg','The scientific name:Aripiprazole used in the treatment of:  - epression with other drugs.  - Treat psychosis  - It improves focus and thinking positively - It helps in alleviating nervousness in the individual');
@@ -112,195 +138,8 @@ new Medicine('Muxava 400mg','17 Jd','images/154963458_1052453095243691_452725355
 new Medicine('januvia 100mg','33.62 Jd','images/155056008_854316428744806_2516997780064909688_n.jpg','The scientific name : Sitagliptin phosphate monohydrate used in the treatment of: It is used with diet and exercise to  control blood sugar in type 2 diabetics.It is not used for type 1 diabetes patients and for treating hyperacidity of the blood with increased ketosis.');
 new Medicine(' Antagra 250mg','21.08 Jd','images/155061320_822452931668496_1962498670354612768_n.jpg','The scientific name : Levetiracetam It is used to prevent epileptic seizures or seizures in the brain by inhibiting certain processes that help in the formation of epileptic seizures, and helps to control them.');
 new Medicine('Zovirax 800mg','30.14 Jd','images/155076816_421620742395338_5530776960820513922_n.jpg',' Scientific name: Acyclovir It is used as a treatment for herpes infection. - The drug is given as an ointment, which helps relieve the severity of the outbreak of ulcers. - This medication is given to people  with compromised immune systems who have a herpes virus infection.');
-new Medicine('Onlefit 120mg','25.93 Jd','images/155089263_254504022958964_6405614336567169049_n.jpg','The scientific name : Orlistat used to -treat obesity, including weight loss and weight control, and is used in conjunction with a low-calorie,  low-fat diet. - It reduces the risk of regaining weight after losing it before.It is used for patients with obesity with an initial body mass index 30 kg / m2 or 27 kg / m2 in the presence of other risk factors (such as diabetes, hyperlipidemia, high blood pressure) .');
+new Medicine('Onlefit 120mg','25.93 Jd','images/155089263_254504022958964_6405614336567169049_n.jpg','The scientific name : Orlistat used to -treat obesity, including weight loss and weight control, and is used in conjunction with a low-calorie,  low-fat diet. - It reduces the risk of regaining weight after losing it before.It is used for patients with obesity with a .');
 new Medicine('sandimmun neoral 100mg','166 Jd','images/download (1).jpg','The scientific name : Cyclosporine used to -prevent rejection of transplanted organs in cases of liver, heart and kidney  transplantation. -Cyclosporine is classified as an immunosuppressant as it reduces the body\'s immunity to a transplanted organ, thus accepting the organ without .');
 new Medicine('Forxiga 10mg','34.48 Jd','images/155189718_272896877555583_204947457729308298_n.jpg','The scientific name : Dapagliflozin It is used to  -control blood sugar levels in patients with type 2 diabetes.-Pharmacological forms of Forsega . -Coated tablets 10 mg, 5 mg .');
 new Medicine('Intense chronic obstructive pulmonary oxygen','900 Jd','images/154362219_845885482626166_7675673876928082082_n.jpg','-A 5-liter household oxygen generating device . -Italian origin .');
 new Medicine('Medical chair for special needs','2000 Jd','images/154328692_791594548121080_2161618573554802077_n.jpg','Medical chair for special needs  Helping patients with disabilities or those with special needs (mobility impairment) to move and move easily and easily.');
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////
-function addToNeed(event){
-  document.location.href='#popup1';
-  event.preventDefault();
-  let a = event.target.id;
-  medicineInNeed.push(medicine[a]);
-  savedData();
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// PatientForm();
-
-// function PatientForm(){
-
-//   let formOne = document.createElement('form');
-//   formOne.id = 'formOne';
-//   let patientName =document.createElement ('input');
-//   patientName.setAttribute('placeholder','your name');
-//   patientName.setAttribute('type','text');
-//   patientName.setAttribute('id','namepatient');
-//   let pTimeOne =document.createElement('input');
-//   pTimeOne.setAttribute('placeholder','When do you want this medication ?');
-//   pTimeOne.setAttribute('type','text');
-//   pTimeOne.setAttribute('id','timepatient');
-//   let pLocation = document.createElement('label');
-//   pLocation.textContent='Location';
-//   pLocation.id = 'label1';
-//   let arrayLoaction= ['Amman' , 'Irbid','Karak' ,'Mafraq', 'Ma\'an' , 'Tafilah', 'Madaba', 'Jerash', 'Ajloun' , 'Aqaba' , 'Zarqa', 'Balqa' ];
-//   let select = document.createElement('select');
-//   select.id = 'optionOne';
-
-
-//   for(let i=0;i<arrayLoaction.length;i++){
-//     let option = document.createElement('option');
-//     select.appendChild(option);
-//     option.value =arrayLoaction[i];
-//     option.textContent= `${arrayLoaction[i]}`;
-//     // option.setAttribute('id','optionOne');
-//   }
-
-//   // let img= document.createElement('label');
-//   // img.textContent='upload your leaflet';
-//   // let uploadImg = document.createElement('input');
-//   // formOne.appendChild(uploadImg);
-//   // uploadImg.setAttribute('type','file');
-//   // uploadImg.setAttribute('id','imageleaflet');
-
-//   let label = document.createElement('label');
-//   label.textContent='is this medicine for you?';
-//   label.id = 'label2';
-//   let radio = document.createElement('input');
-
-//   radio.setAttribute('type','radio');
-//   radio.setAttribute('name','options');
-//   radio.setAttribute('id','radioOne');
-//   let yesOption =document.createElement('label');
-//   yesOption.textContent= 'Yes' ;
-//   yesOption.id = 'label3';
-//   let noOption=document.createElement('input');
-//   noOption.setAttribute('type','radio');
-//   noOption.setAttribute('name','options');
-//   noOption.setAttribute('id','radioTwo');
-//   let labelTwo= document.createElement('label');
-//   labelTwo.textContent='No';
-//   labelTwo.id = 'label4';
-
-
-
-//   let buttonEL2 =document.createElement('input');
-//   formOne.appendChild(buttonEL2);
-//   buttonEL2.textContent='Save Information';
-//   buttonEL2.setAttribute('type','submit');
-//   buttonEL2.setAttribute('value','submit');
-//   buttonEL2.setAttribute('id','id2');
-//   buttonEL2.setAttribute('onclick', 'sweetAl()');
-//   let closeBtn = document.createElement('input');
-//   closeBtn.setAttribute ('onclick','closeFun()');
-//   closeBtn.textContent = 'close';
-//   formOne.appendChild(closeBtn);
-//   closeBtn.type = 'button';
-//   closeBtn.value = 'close';
-//   closeBtn.id = 'closeBtn';
-//   formOne.appendChild(patientName);
-//   formOne.appendChild(pTimeOne);
-//   formOne.appendChild(pLocation);
-//   formOne.appendChild(select);
-//   formOne.appendChild(label);
-//   formOne.appendChild(radio);
-//   formOne.appendChild(yesOption);
-//   formOne.appendChild(noOption);
-//   formOne.appendChild(labelTwo);
-//   formOne.appendChild(buttonEL2);
-
-
-
-//   formOne.addEventListener('submit',addToForm);
-
-//   abc.appendChild(formOne);
-
-
-// }
-
-
-
-
-let arrayForm = [];
-
-function Form(patientNam,idphone, yourLocation){
-  this.patientNam = patientNam;
-  // this.time = time;
-  // this.medicineForYou = medicineForYou;
-  this.location = yourLocation;
-  this.phonepat=idphone;
-  arrayForm.push(this);
-  console.log(this);
-}
-
-function addToForm(event){
-  event.preventDefault();
-  console.log('1');
-  console.log(event);
-  let idName = event.target.namepatient.value;
-  let idLocation = event.target.ploc.value;
-  let idphone = event.target.patientPhone.value;
-  new Form (idName , idphone, idLocation );
-  document.location.href='#';
-  savedForm();
-}
-
-function savedForm(){
-  console.log(arrayForm);
-  let data = JSON.stringify(arrayForm);
-  localStorage.setItem('forminformation', data);
-
-}
-
-
-
-
-
-function closeForm() {
-  document.getElementById('myForm').style.display = 'none';
-}
-
-
-
-let myForm = document.getElementById('myForm');
-let medicineName , imgSource='http://www.imgworlds.com/wp-content/uploads/2015/12/18-CONTACTUS-HEADER.jpg' ,price=0 , info=' ';
-
-
-function openForm() {
-
-  let elem = document.getElementById('myForm');
-  elem.style.display = 'block';
-  myForm.addEventListener('submit',function newLocal(event){
-    event.preventDefault();
-    console.log(event);
-    medicineName = event.target.medicineName.value;
-    imgSource = event.target.source.value;
-    price = parseInt(event.target.price.value);
-    info =event.target.info.value;
-    new Medicine(medicineName , price , imgSource , info);
-
-    myForm.removeEventListener('submit',newLocal);
-    closeForm();
-  });
-
-}
-
-
-
